e5a898ebaff7e5f731769f4aece51ce4bae70695
shubhampadhy512 <shubhampadhy512@gmail.com>
2026-01-30 21:49:20 +0530

    added the movement right and left

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    Vector2 moveInput;
+    public bool IsMoving { get; private set; }
+    public float speed = 5f;
+
+    Rigidbody2D rg;
+
+    private void Awake()
     {
-        
+        rg = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rg.linearVelocity = new Vector2(
+            moveInput.x * speed,
+            rg.linearVelocity.y
+        );
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
     }
 }
