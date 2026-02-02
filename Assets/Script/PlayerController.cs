293203a043f5c8d0b5ffab274139647c875d4a6d
shubhampadhy512 <shubhampadhy512@gmail.com>
2026-02-02 23:03:17 +0530

    animation of left and right

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,16 +4,49 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    
+
     Vector2 moveInput;
-    public bool IsMoving { get; private set; }
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1,1);
+            }
+            _isFacingRight=value;
+        }
+    }
+
     public float speed = 5f;
 
+    Animator animator;
     Rigidbody2D rg;
 
     private void Awake()
     {
         rg = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -28,5 +61,17 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
         IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
     }
 }
